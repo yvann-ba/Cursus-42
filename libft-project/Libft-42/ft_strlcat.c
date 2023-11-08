feda658e06e51757e520c856f7e8fc38a4445dec
yvann-hub <barbot.yvann@gmail.com>
2023-11-08 08:55:03 +0100

    l

--- libft-project/Libft-42/ft_strlcat.c
+++ libft-project/Libft-42/ft_strlcat.c
@@ -1,42 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybarbot <ybarbot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 10:00:53 by ybarbot           #+#    #+#             */
-/*   Updated: 2023/11/08 08:42:52 by ybarbot          ###   ########.fr       */
+/*   Created: 2023/11/08 08:08:50 by ybarbot           #+#    #+#             */
+/*   Updated: 2023/11/08 08:49:02 by ybarbot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"   
+#include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 	size_t	j;
-	size_t	is_present;
 
-	if (little[0] == '\0' || len == 0)
-		return ((char *)big);
+	if ((dst == NULL || src == NULL) && size == 0)
+		return (0);
 	i = 0;
-	while (big[i] && i < len)
+	while (dst[i] && i < size)
+		i++;
+	j = 0;
+	if (size > 0)
 	{
-		j = 0;
-		while (little[j] && i + j < len)
+		while (src[j] && i + j < size - 1)
 		{
-			if (little[j] != big[j + i])
-			{
-				is_present = 0;
-				break ;
-			}
-			is_present = 1;
+			dst[i + j] = src[j];
 			j++;
 		}
-		if (is_present == 1 && little[j] == '\0')
-			return ((char *)big + i);
-		i++;
+		dst[i + j] = '\0';
 	}
-	return (0);
+	return (i + j);
 }
+
+
