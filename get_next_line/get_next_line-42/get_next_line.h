d42aa91fafbda2b2b33d4271fc6b019730728f0b
yvann-hub <barbot.yvann@gmail.com>
2023-11-21 10:13:43 +0100

    new project gnl

---
+++ get_next_line/get_next_line-42/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybarbot <ybarbot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 10:08:33 by ybarbot           #+#    #+#             */
+/*   Updated: 2023/11/21 10:11:04 by ybarbot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE.H
+# define GET_NEXT_LINE.H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
