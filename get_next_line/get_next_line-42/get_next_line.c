dc0d4d717af0f5fbfa8171bc156f9f4e8643ca4d
yvann-hub <barbot.yvann@gmail.com>
2023-11-22 13:29:09 +0100

    start prototype gnl

--- get_next_line/get_next_line-42/get_next_line.c
+++ get_next_line/get_next_line-42/get_next_line.c
@@ -6,11 +6,24 @@
 /*   By: ybarbot <ybarbot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 10:08:07 by ybarbot           #+#    #+#             */
-/*   Updated: 2023/11/21 10:09:43 by ybarbot          ###   ########.fr       */
+/*   Updated: 2023/11/22 13:13:07 by ybarbot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd)
 {
-
+	void *buf;
+	static char *stash;
+	
+	buf = malloc(BUFFER_SIZE + 1);
+	if (buf == NULL)
+		return (NULL);
+	while(i != '\n')
+	{
+		if (read(fd, buf, BUFFER_SIZE ) == 0)
+			break;
+		j = 0;
+		stash[i] = buf [i]
+	}
+	return (stash);
 }
